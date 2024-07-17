0b4b7e07e1f7826522a71461f257d385a9a9688e
MostafaElKaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2024-07-17 19:52:40 +0300

    remove comments

--- Backend/src/index.mjs
+++ Backend/src/index.mjs
@@ -1,31 +1,7 @@
 import express from 'express';
 
-// import user routers
-// import signIn from './routers/userManagement/signIn.mjs';
-// import signUp from './routers/userManagement/signUp.mjs';
-
-// // import posts routers
-// import createPost from './routers/postManagement/createPost.mjs';
-// import deletePost from './routers/postManagement/deletePost.mjs';
-// import updatePost from './routers/postManagement/updatePost.mjs';
-// import createComment from './routers/postManagement/createComment.mjs';
-// import updateComment from './routers/postManagement/updateComment.mjs';
-// import deleteComment from './routers/postManagement/deleteComment.mjs';
-
 const app  = express();
 app.use(express.json());
-
-// user routers
-// app.use(signIn);
-// app.use(signUp);
-
-// // posts routers
-// app.use(createPost);
-// app.use(deletePost);
-// app.use(updatePost);
-// app.use(createComment);
-// app.use(updateComment);
-// app.use(deleteComment);
 
 import authRouter from './routers/authRouter.mjs';
 import postRouter from './routers/postRouter.mjs';
