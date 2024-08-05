ac7317b2b273d0ba9aaefed5c6598d9d428b34d4
MostafaElKaranshawy <110842052+MostafaElKaranshawy@users.noreply.github.com>
2024-08-05 22:01:40 +0300

    Add AuthValidator as middleware

---
+++ Backend/src/middleWares/authValidation.mjs
@@ -0,0 +1,39 @@
+import { body, validationResult } from 'express-validator';
+
+const emailValidation = body('email')
+    .isString()
+    .notEmpty()
+    .withMessage('Email must not be empty')
+    .isEmail()
+    .withMessage('Must be a valid email');
+
+const passwordValidation = body('password')
+    .isString()
+    .notEmpty()
+    .withMessage('Password must not be empty')
+    .isLength({ min: 8, max: 20 })
+    .withMessage('Password must be between 8 and 20 characters');
+
+const nameValidation = body('name')
+    .isString()
+    .notEmpty()
+    .withMessage('Name must not be empty')
+    .isLength({ min: 5, max: 30 })
+    .withMessage('Name must be between 5 and 30 characters');
+
+const userNameValidation = body('username')
+    .isString()
+    .notEmpty()
+    .withMessage('Username must not be empty')
+    .isLength({ min: 5, max: 30 })
+    .withMessage('Username must be between 5 and 30 characters');
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+
+export default { emailValidation, passwordValidation, nameValidation, userNameValidation, validate };
--- Backend/src/routers/authRouter.mjs
+++ Backend/src/routers/authRouter.mjs
@@ -1,60 +1,24 @@
 import express from 'express';
-import { body, validationResult } from 'express-validator';
 import AuthController from '../controllers/authController.mjs';
+import AuthValidator from '../middleWares/authValidation.mjs';
 
 const authRouter = express.Router();
 
-const emailValidation = body('email')
-    .isString()
-    .notEmpty()
-    .withMessage('Email must not be empty')
-    .isEmail()
-    .withMessage('Must be a valid email');
-
-const passwordValidation = body('password')
-    .isString()
-    .notEmpty()
-    .withMessage('Password must not be empty')
-    .isLength({ min: 8, max: 20 })
-    .withMessage('Password must be between 8 and 20 characters');
-
-const nameValidation = body('name')
-    .isString()
-    .notEmpty()
-    .withMessage('Name must not be empty')
-    .isLength({ min: 5, max: 30 })
-    .withMessage('Name must be between 5 and 30 characters');
-
-const userNameValidation = body('username')
-    .isString()
-    .notEmpty()
-    .withMessage('Username must not be empty')
-    .isLength({ min: 5, max: 30 })
-    .withMessage('Username must be between 5 and 30 characters');
-
-const validate = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-};
-
 authRouter.post(
     '/signUp',
-    emailValidation,
-    passwordValidation,
-    nameValidation,
-    userNameValidation,
-    validate,
+    AuthValidator.emailValidation,
+    AuthValidator.passwordValidation,
+    AuthValidator.nameValidation,
+    AuthValidator.userNameValidation,
+    AuthValidator.validate,
     AuthController.signUp
 );
 
 authRouter.get(
     '/signIn',
-    emailValidation,
-    passwordValidation,
-    validate,
+    AuthValidator.emailValidation,
+    AuthValidator.passwordValidation,
+    AuthValidator.validate,
     AuthController.signIn
 );
 
